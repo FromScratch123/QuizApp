fa2a89266c0e48da566bb6835e59e9898f5791df
Tatsuya <hctarcsmorf@gmail.com>
2020-01-13 10:19:17 +0900

    quizSet was made

--- js/main.js
+++ js/main.js
@@ -1,5 +1,16 @@
- const price = [10,80,30,50];
- price.splice(1,2);
- console.log(price);
- price.splice(1,0,80,60,30);
- console.log(price);
+'use strict';
+
+{
+ const question = document.getElementById('question');
+ const choices = document.getElementById('choices');
+ const btn = document.getElementById('btn');
+
+ const quizSet = [
+   {q: 'What is A?', c:['A0', 'A1', 'A2']},
+   {q: 'What is B?', c:['B0', 'B1', 'B2']},
+   {q: 'What is C?', c:['C0', 'C1', 'C2']},
+ ];
+
+ let currentNum = 0;
+  
+}
